c068609dee78d89ebe91743d4b3b969dff8fec10
Tetiana Poluiko <tanya911911@gmail.com>
2026-08-25 07:26:38 +0200

    Filter-Suche-Fertig

--- src/app/components/produktkatalog/produktkatalog.ts
+++ src/app/components/produktkatalog/produktkatalog.ts
@@ -16,24 +16,39 @@ export class ProduktkatalogComponent {
    // Список товаров, который выводится на экран (меняется при фильтрации)
    filteredProducts: Product[] = PRODUCTS;
 
-  // Переменные для фильтров
+  // Переменные для хранения значений фильтров и поиска
   selectedCategory: string = '';
   minPrice: number | null = null;
   maxPrice: number | null = null;
-
+  
   // Функция фильтрации
   applyFilter() {
   this.filteredProducts = this.products.filter(item => {
   // Проверка категории
-  const matchesCategory = !this.selectedCategory || item.category === this.selectedCategory;
+  const matchesCategory = !this.selectedCategory || 
+  item.category === this.selectedCategory;
 
   // Проверка минимальной цены
-  const matchesMin = this.minPrice === null || item.price >= this.minPrice;
+  const matchesMin = this.minPrice === null || 
+  item.price >= this.minPrice;
 
   // Проверка максимальной цены
-  const matchesMax = this.maxPrice === null || item.price <= this.maxPrice;
+  const matchesMax = this.maxPrice === null || 
+  item.price <= this.maxPrice;
 
   return matchesCategory && matchesMin && matchesMax;
   });
  }
+   // Функция поиска
+   searchTerm: string = '';
+
+  applySuche() {
+  this.filteredProducts = this.products.filter(item => {
+    //приводим название товара и поисковый запрос к нижнему регистру
+    //чтобы поиск работал независимо от больших и маленьких букв
+    //и поиск выполняется по включению (includes) фразы в название товара
+    return item.name.toLowerCase().includes(this.searchTerm.toLowerCase());
+  });
+ }
+
 }
