4d85e468c5b9520cbb84ab6e57a1e20c2a64b92e
Tetiana Poluiko <tanya911911@gmail.com>
2026-08-26 21:57:50 +0200

    Home-Button-Details

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+    constructor (private router:Router){}
+    applyDetail(id:number){
+      this.router.navigate(['/produkt-card',id]);
+  }
+}
